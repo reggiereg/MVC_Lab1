31bae1e9c16a0b47f117faca1569b5f10f367700
Reginald Richardson <youngreg21@gmail.com>
2020-02-23 23:13:57 -0500

    First commit of MVC Lab

---
+++ Models/Tripometer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab.Models
+{
+    public class Tripometer
+    {
+        public int MPG { get; set; }
+        public int Gallons { get; set; }
+        public int TripDistance { get; set; }
+        public int Range { get; set; }
+
+        public Tripometer(int mpg, int gallons)
+        {
+            MPG = mpg;
+            Gallons = gallons;
+        }
+
+        public double SetRange(int mpg, int gallons)
+        {
+            Range = mpg * gallons;
+            return Range;
+        }
+
+    }
+}
